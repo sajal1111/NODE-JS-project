6cdfd6a5b249f7dfc2f60727b6dcbe278d249104
sajal1111 <103333121+sajal1111@users.noreply.github.com>
2025-08-01 10:00:54 +0530

    Update server-project.js

--- server-project.js
+++ server-project.js
@@ -9,7 +9,7 @@ app.listen(2022, function () {
 })
 const transporter = nodemailer.createTransport({
     service: 'gmail',
-    host: "smtp.gmail.email",
+    host: "smtp.gmail.com",
     port: 587,
     secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
